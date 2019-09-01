f3b8311c1f8f86834f5a25e2cb466ddc7c63a449
canvasT <fengtaotao082913@gmail.com>
2019-09-01 22:02:08 +0800

    add: change theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,8 +58,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Tao's blog`,
+        short_name: `Tao's blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Github from "typography-theme-github"
 
-Wordpress2016.overrideThemeStyles = () => {
+Github.overrideThemeStyles = () => {
   return {
     "a.gatsby-resp-image-link": {
       boxShadow: `none`,
@@ -9,9 +9,9 @@ Wordpress2016.overrideThemeStyles = () => {
   }
 }
 
-delete Wordpress2016.googleFonts
+delete Github.googleFonts
 
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(Github)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
